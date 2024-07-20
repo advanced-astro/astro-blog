56e6ddd79c08369b95aae0e588728c33635a959b
Marc Redwerkz <redwerkz@gmail.com>
2024-07-20 11:32:27 +0200

    deploy to vercel

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Astro Starter Kit: Minimal
+# Astro Weblog
 
 ```sh
 npm create astro@latest -- --template minimal
--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-blog-x2sw.vercel.app",
+  site: "https://aweblog.vercel.app",
   integrations: [tailwind(), react(), mdx()],
   image: {
     domains: ["https://github.com"],
